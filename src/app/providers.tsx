e8e2e77b8f89b169e28e08a2ade074c62fec30c1
ramMahender16 <ram.mahender@gmail.com>
2026-02-21 10:41:37 +0530

    default theme change

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -8,7 +8,7 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       <SWRConfig
         value={{
           fetcher,
